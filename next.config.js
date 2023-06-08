f93c0ea80a0a0713e49d8c4417f3d4b4bb79186f
s.manno <selene.manno@digitouch.it>
2023-06-08 03:35:25 +0200

    feat(ConfigFiles): add basic config of the project's boilerplate

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@tremor/react"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    SPOTIFY_CLIENT_ID:'80679421247947519530146409c7d888',
+    SPOTIFY_CLIENT_SECRET:'4a685ec9cae846c59e1b867c04c2e04c'
+  },
+};
