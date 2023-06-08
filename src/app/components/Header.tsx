8468370a065116b6bcf14b8a473f0286fa9e9af9
s.manno <selene.manno@digitouch.it>
2023-06-08 16:08:11 +0200

    feat(Components): add Revenue component, adjust Header, remove Card

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,29 @@
 import { Card, Metric, Text, Icon, Flex } from "@tremor/react";
 
 import { MusicalNoteIcon, PlayIcon, TicketIcon } from "@heroicons/react/24/outline";
+import { JsxElement } from "typescript";
 
-
+interface Categories  {
+    title: string;
+    metric: number;
+    icon: JsxElement;
+    color: string;
+}
 const categories = [
   {
-    title: "Assets",
-    metric: "$ 23,456,456",
+    title: "Tracks",
+    metric: "23,456,456",
     icon: MusicalNoteIcon,
+    color: "purple",
   },
   {
-    title: "Tax",
-    metric: "$ 13,123",
+    title: "Shows",
+    metric: "13",
     icon: TicketIcon,
     color: "yellow",
   },
   {
-    title: "Employees",
+    title: "Plays",
     metric: "456",
     icon: PlayIcon,
     color: "red",
@@ -26,13 +33,11 @@ function Header() {
   return (
     <>
       {categories.map((item) => (
-        <Card key={item.title} decoration="top" className={item.color}>
-          <Flex className="justify-start gap-x-4">
-            <Icon icon={item.icon} variant="simple" size="sm" color="purple"/>
-            {/* <Block truncate={true}> */}
+        <Card key={item.title} decoration="top">
+          <Flex className="justify-start gap-x-8">
+            <Icon icon={item.icon} variant="simple" size="sm"/>
               <Text>{item.title}</Text>
               <Metric>{item.metric}</Metric>
-            {/* </Block> */}
           </Flex>
         </Card>
       ))}
